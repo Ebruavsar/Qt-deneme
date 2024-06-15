b8d3286c9db94b23988704dfb1db5094073d24a0
Ebruavsar <73585933+Ebruavsar@users.noreply.github.com>
2024-06-15 09:22:46 +0300

    1

---
+++ HelloWorld/mainwindow.cpp
@@ -0,0 +1,23 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::on_pushButton_clicked);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox::information(this, "Hello", "Hello, World!");
+}
+
