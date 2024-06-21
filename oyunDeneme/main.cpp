da902a1d793e0859f937f58949d5fd407fef7bc9
Ebruavsar <73585933+Ebruavsar@users.noreply.github.com>
2024-06-21 13:24:07 +0300

    5

---
+++ oyunDeneme/main.cpp
@@ -0,0 +1,30 @@
+
+#include <QApplication>
+#include "MyRect.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //create a scane(sahne)
+    QGraphicsScene * scane =new QGraphicsScene();
+
+    //create an item to put into the scene(öğe)
+    MyRect * rect = new MyRect();
+    rect->setRect(0,0,100,100);
+
+    //add the item to the scene
+    scane->addItem(rect);
+
+    //make rect focusable
+    rect->setFlag(QGraphicsItem::ItemIsFocusable);
+    rect->setFocus();
+
+    //add a view
+    QGraphicsView * view =new QGraphicsView(scane);
+    view->show();
+
+    return a.exec();
+}
