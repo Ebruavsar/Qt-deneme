844a327976507fe8771e4992286633b97df078a6
Ebruavsar <73585933+Ebruavsar@users.noreply.github.com>
2024-06-23 14:54:15 +0300

    -

--- oyunDeneme/Bullet.cpp
+++ oyunDeneme/Bullet.cpp
@@ -1,6 +1,8 @@
 #include "Bullet.h"
 #include <QTimer>
 #include <QGraphicsScene>
+#include <QList>
+#include "Enemy.h"
 Bullet::Bullet(): QObject(), QGraphicsRectItem(){
     //drew the rect
     setRect(0,0,10,50);
@@ -14,7 +16,22 @@ Bullet::Bullet(): QObject(), QGraphicsRectItem(){
 
 void Bullet::move()
 {
-   //move bullet up
+    //if bullet colliides with enemy, deatroy both
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+    for(int i=0,n = colliding_items.size() ; i<n; ++i ){
+        if (typeid(*(colliding_items[i]))==typeid(Enemy)){
+            //remove them both
+            scene()->removeItem(colliding_items[i]);
+            scene()->removeItem(this);
+            //delete them both
+            delete colliding_items[i];
+            delete this;
+            return;
+
+        }
+    }
+
+    //move bullet up
     setPos(x(),y()-10);
     if(pos().y()+rect().height() <0){
         scene()->removeItem(this);
